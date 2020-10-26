6bcba361ebba9b22bab4a198f815ed2c8c3cc57c
cduboff <clbduboff@gmail.com>
2020-10-25 22:27:05 -0400

    adding functionality

--- app_one/views.py
+++ app_one/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("This is my webpage")
+    return render(request, 'index.html')
+
+def submit(request):
+    return HttpResponse("This is a placeholder for processing and returning the API response")

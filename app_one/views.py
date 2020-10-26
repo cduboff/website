9197b62764ab3616d377d4ba3eb02b7981a6ff8e
cduboff <clbduboff@gmail.com>
2020-10-26 12:11:02 -0400

    api updates

--- app_one/views.py
+++ app_one/views.py
@@ -13,6 +13,11 @@ def submit(request):
     print(request.POST['ingredients'])
     api_response = requests.get(f'https://api.spoonacular.com/recipes/findByIngredients?apiKey=242edaca2243437482a5374c764c9098&ingredients={q}').json()
     request.session['recipes'] = api_response
+    for recipe in api_response:
+        print(recipe['id'])
+        # use this id to send another request to the api to retreive the actual recipe information. decide whether this is accessible immediately after submitting ingredients or if user will click image for more info about the recipe
+    # for i in request.session['recipes']:
+    #     requests.get(f'https://api.spoonacular.com/recipes/{i[id]}/information?apiKey=242edaca2243437482a5374c764c9098/')
     return redirect(f'/response')
 
 def response(request):

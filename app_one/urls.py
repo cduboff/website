f6f1812cf72cc8666900892c0e2ccdb303c5f2c2
cduboff <clbduboff@gmail.com>
2020-10-26 20:47:20 -0400

    fixing single recipe search

--- app_one/urls.py
+++ app_one/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('search', views.search),
+    path('search/<int:id>', views.searchByid),
     path('submit', views.submit),
     path('response', views.response),
     path('create_meal_plan', views.create),
--- app_one/views.py
+++ app_one/views.py
@@ -14,18 +14,28 @@ def submit(request):
     n = request.POST['number']
     api_response = requests.get(f'https://api.spoonacular.com/recipes/findByIngredients?apiKey=242edaca2243437482a5374c764c9098&ingredients={q}&{n}').json()
     request.session['recipes'] = api_response
-    for recipe in api_response:
+    for recipe in request.session['recipes']:
         print(recipe['id'])
+        print(recipe)
+        # request.session['ingredients'] = requests.get(f'https://api.spoonacular.com/recipes/{recipe[id]}/information?apiKey=242edaca2243437482a5374c764c9098/')
         # use this id to send another request to the api to retreive the actual recipe information. decide whether this is accessible immediately after submitting ingredients or if user will click image for more info about the recipe
     # for i in request.session['recipes']:
-    #     requests.get(f'https://api.spoonacular.com/recipes/{i[id]}/information?apiKey=242edaca2243437482a5374c764c9098/')
     return redirect(f'/response')
 
 def response(request):
     context = {
         'recipes': request.session['recipes'],
+        # 'ingredients': request.session['ingredients'],
     }
     return render(request, 'response.html', context)
+
+def searchByid(request, id):
+    response = requests.get(f'https://api.spoonacular.com/recipes/{id}/information?apiKey=242edaca2243437482a5374c764c9098').json()
+    print(response)
+    context = {
+        'response': response,
+    }
+    return render(request, 'one_recipe.html', context)
 
 def create(request):
     return render(request, 'create_plan.html')
